2588cbf33fcbc6ab52ebff633a2a0d797e5a223b
Simran <sksimran020@gmail.com>
2022-10-16 02:15:35 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -3,11 +3,28 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import UsePullRequest from './components/usePullRequest';
 
 const App = () => {
+  const myStyle = {
+    color: "white",
+    backgroundColor: "DodgerBlue",
+    padding: "10px",
+    fontFamily: "Sans-Serif"
+  };
+
+  const divStyle = {
+    display:"flexbox" , 
+    alignItems:"center" , 
+    justifyContent:"center" , 
+    marginLeft:"auto" , 
+    marginRight:"auto",
+    borderRadius: "35px"
+  };
+  
   return (
 <>
+<div className="w-50 p-3 rounded" style={divStyle}>
+  <h1 style={myStyle} className="text-center">Neovim Repoistory Pull Requests</h1></div>
 <UsePullRequest/>
 </>
     )
-}
-
+  }
 export default App;
--- src/components/usePullRequest.js
+++ src/components/usePullRequest.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { FaGithubSquare} from "react-icons/fa";
+import { FaGithubSquare } from "react-icons/fa";
 import * as ReactBootStrap from "react-bootstrap";
 import InfiniteScroll from "react-infinite-scroll-component";
 
@@ -8,21 +8,23 @@ const UsePullRequest = () => {
   const [hasMore, sethasMore] = useState(true);
   const [page, setpage] = useState(2);
 
-
+  const divStyle = {
+    marginLeft:"auto" , 
+    marginRight:"auto"
+  }
   const listOfRequests = async () => {
-      const result = await fetch(
-        `https://api.github.com/repos/neovim/neovim/pulls?state=all&per=1&per_page=100&limit=30`
-      );
-      const list = await result.json();
-      console.log(list);
-        setState(list);
-      // console.log(result.data);
+    const result = await fetch(
+      `https://api.github.com/repos/neovim/neovim/pulls?state=all&per=1&per_page=100&limit=30`
+    );
+    const list = await result.json();
+    console.log(list);
+    setState(list);
+    // console.log(result.data);
   };
 
   useEffect(() => {
     listOfRequests();
   }, []);
-
 
   const fetchList = async () => {
     const res = await fetch(
@@ -32,22 +34,20 @@ const UsePullRequest = () => {
     return data;
   };
 
-
   const fetchData = async () => {
     const prevData = await fetchList();
-    setState([...state , ...prevData]);
+    setState([...state, ...prevData]);
     if (prevData.length === 0 || prevData.length < 400) {
-      sethasMore(false); 
+      sethasMore(false);
       console.log("Printing");
     }
     setpage(page + 1);
-  }
-
+  };
 
   const pullRequests =
     state.length !== 0
-      ? state.map((item) => (
-          <tr key={item.id}>
+      ? state.map((item, index) => (
+          <tr key={index}>
             <td>{item.title}</td>
             <td>{item.base.repo.default_branch}</td>
             <td>-</td>
@@ -68,35 +68,32 @@ const UsePullRequest = () => {
         ))
       : console.log("No Pull Requests Found");
 
-
-      
-      
   return (
     <>
-      
       <InfiniteScroll
         dataLength={state.length} //This is important field to render the next data
         next={fetchData}
         hasMore={hasMore}
-        loader={"Loading..." }
+        loader={"Loading..."}
       >
+        <div className="w-90 p-3 shadow-sm p-3 mb-5 bg-white rounded" style={divStyle}>
         <ReactBootStrap.Table striped bordered hover responsive="sm">
           <thead>
             <tr>
               <th>Title</th>
-              <th>Base Branch</th>
-              <th>Author Branch</th>
+              <th className="text-nowrap">Base Branch</th>
+              <th className="text-nowrap">Author Branch</th>
               <th>Author</th>
-              <th>Created On</th>
+              <th className="text-nowrap">Created On</th>
               <th>Reviewers</th>
               <th>Labels</th>
               <th>URL</th>
             </tr>
           </thead>
-          <tbody>
-        {pullRequests}
-        </tbody>
+          <tbody>{pullRequests}</tbody>
         </ReactBootStrap.Table>
+        </div>
+        
       </InfiniteScroll>
     </>
   );
